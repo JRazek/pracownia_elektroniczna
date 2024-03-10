3d23953e8a1d20565d7aa3ad9dfb032b74db99d9
JRazek <jakub.razek2@gmail.com>
2024-03-10 02:46:55 +0100

    working test and expected_value/variance

--- src/measurements_uncertainties.rs
+++ src/measurements_uncertainties.rs
@@ -1,44 +1,71 @@
 use super::integral::Integral;
 
 pub struct DistributionStats {
-    mean: f64,
+    expected_value: f64,
     variance: f64,
 }
 
-pub struct CompactlySupportedPDF<F> {
+pub struct CompactlySupportedDistribution<F, PDF> {
     pub support: (f64, f64),
-    pub pdf: F,
+    pub f: F,
+    pub pdf: PDF,
 }
 
-impl<PDF> CompactlySupportedPDF<PDF>
+impl<F, PDF> CompactlySupportedDistribution<F, PDF>
 where
+    F: Fn(f64) -> f64,
     PDF: Fn(f64) -> f64,
 {
-    pub fn new(support: (f64, f64), pdf: PDF) -> Self {
-        Self { support, pdf }
+    pub fn new(support: (f64, f64), f: F, pdf: PDF) -> Self {
+        Self { support, f, pdf }
     }
 
-    pub fn transform<F>(&self, f: F) -> CompactlySupportedPDF<impl Fn(f64) -> f64 + Clone>
+    pub fn transform_x<G>(
+        &self,
+        g: G,
+    ) -> CompactlySupportedDistribution<impl Fn(f64) -> f64 + Clone, PDF>
     where
-        F: Fn(f64) -> f64 + Clone,
+        G: Fn(f64) -> f64 + Clone,
         PDF: Clone,
+        F: Clone,
     {
         let support = self.support;
         let pdf = self.pdf.clone();
+        let f = self.f.clone();
 
-        CompactlySupportedPDF::new(support, move |x| pdf(f(x)))
+        CompactlySupportedDistribution::new(support, move |x| g(f(x)), pdf)
     }
 
-    pub fn calculate_distribution<I>(&self, integral: I) -> DistributionStats
+    pub fn expected_value<I>(&self, integral: &I) -> f64
     where
         I: Integral,
         PDF: Clone,
+        F: Clone,
     {
-        let mean = integral.integrate(&self.pdf, self.support);
-        let transformed_pdf = self.transform(|x: f64| x.powi(2) as f64);
-        let variance = integral.integrate(&transformed_pdf.pdf, self.support) - mean.powi(2);
+        let pdf = self.pdf.clone();
+        let f = self.f.clone();
 
-        DistributionStats { mean, variance }
+        let integrand = |x: f64| f(x) * pdf(x);
+        integral.integrate(integrand, self.support)
+    }
+
+    pub fn calculate_distribution<I>(&self, integral: &I) -> DistributionStats
+    where
+        I: Integral,
+        PDF: Clone,
+        F: Clone,
+    {
+        let expected_value = self.expected_value(integral);
+
+        let transformed_distribution = self.transform_x(|x: f64| x.powi(2) as f64);
+        let transformed_expected_value = transformed_distribution.expected_value(integral);
+
+        let variance = transformed_expected_value - expected_value.powi(2);
+
+        DistributionStats {
+            expected_value,
+            variance,
+        }
     }
 }
 
@@ -47,7 +74,13 @@ struct EmptyRangeError;
 
 fn uniform_distribution_pdf(
     support: (f64, f64),
-) -> Result<CompactlySupportedPDF<impl Fn(f64) -> f64 + Copy + 'static>, EmptyRangeError> {
+) -> Result<
+    CompactlySupportedDistribution<
+        impl Fn(f64) -> f64 + Copy + Clone + 'static,
+        impl Fn(f64) -> f64 + Copy + Clone + 'static,
+    >,
+    EmptyRangeError,
+> {
     let (a, b) = support.clone();
     let range = a..b;
 
@@ -57,10 +90,16 @@ fn uniform_distribution_pdf(
 
     let pdf = move |x: f64| {
         let range = a..b;
-        (if range.contains(&x) { 1. } else { 0. }) / 2. * (b - a) as f64
+        (if range.contains(&x) { 1. } else { 0. }) / (b - a) as f64
     };
 
-    Ok(CompactlySupportedPDF { support, pdf })
+    let identity = |x: f64| x;
+
+    Ok(CompactlySupportedDistribution {
+        support,
+        f: identity,
+        pdf,
+    })
 }
 
 fn uniform_distribution_stats(support: (f64, f64)) -> Result<DistributionStats, EmptyRangeError> {
@@ -74,7 +113,10 @@ fn uniform_distribution_stats(support: (f64, f64)) -> Result<DistributionStats, 
     let mean = (a + b) / 2.;
     let variance = (b - a).powi(2) / 12.;
 
-    Ok(DistributionStats { mean, variance })
+    Ok(DistributionStats {
+        expected_value: mean,
+        variance,
+    })
 }
 
 #[cfg(test)]
@@ -88,13 +130,17 @@ mod tests {
     #[test]
     fn test_uniform_distribution() {
         let uniform = uniform_distribution_pdf((0., 1.)).unwrap();
-        let transformed_uniform = uniform.transform(|x: f64| x.powi(2) as f64);
+        let transformed_uniform = uniform.transform_x(|x: f64| x as f64);
 
-        let stats = transformed_uniform.calculate_distribution(RiemannSum { n: 1000 });
+        let stats = transformed_uniform.calculate_distribution(&RiemannSum { n: 1000 });
 
         let defined_stats = uniform_distribution_stats((0., 1.)).unwrap();
 
-        assert_relative_eq!(stats.mean, defined_stats.mean);
-        assert_relative_eq!(stats.variance, defined_stats.variance);
+        assert_relative_eq!(
+            stats.expected_value,
+            defined_stats.expected_value,
+            epsilon = 1e-2
+        );
+        assert_relative_eq!(stats.variance, defined_stats.variance, epsilon = 1e-2);
     }
 }
